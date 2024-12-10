235921c38e8d2c6dad190b1c5bfdf1ed857b6805
Hannes Schwanzer <hannes.schwanzer@student.hs-rm.de>
2024-12-10 16:44:17 +0100

    feat: Added dotfiles

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/dotfiles
+CONFIGS=("zsh" "nvim" "tmux" "vscode")
+
+cd $DOTFILES_DIR
+
+for config in "${CONFIGS[@]}"; do
+    echo "Stowing $config..."
+    stow -D $config  # Unstow (if any existing links)
+    stow $config     # Stow the current config
+done
+
+echo "All dotfiles stowed!"
