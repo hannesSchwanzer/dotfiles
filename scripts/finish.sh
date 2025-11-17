aadac4d0afbdb6b3c7cb64f487384e110f20b2b2
Hannes Schwanzer <hannes.schwanzer@student.hs-rm.de>
2025-11-17 18:30:21 +0100

    fix: Threw error, when tmux was freshly installed and no sessions existed

--- scripts/finish.sh
+++ scripts/finish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Reload tmux configuration and install plugins
+tmux new-session -d -s temp || exit 1
 tmux source-file ~/.tmux.conf
 tmux new-session -d 'tmux source-file ~/.tmux.conf; tmux run-shell ~/.tmux/plugins/tpm/bin/install_plugins'
+tmux kill-session -t temp
